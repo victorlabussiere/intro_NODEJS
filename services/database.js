3559cc3101494ab3553df846fa7a477edf10ece4
Victor Labussiere <victor.labussiere95@gmail.com>
2022-10-25 16:43:49 -0300

    update files

--- services/database.js
+++ services/database.js
@@ -5,30 +5,20 @@ const readFileAsync = promisify(readFile)
 class Database {
 
     constructor() {
-
         this.NOME_ARQUIVO = '../services/herois.json'
-
     }
 
     async obterDadosArquivo() {
-
         let arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         let resultado = JSON.parse(arquivo.toString())
         return resultado
 
     }
 
-    // escreverArquivo() {
-
-    // }
-
     async listar(id) {
-
         const dados = await this.obterDadosArquivo()
         const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
-        console.log(dados)
         return dadosFiltrados
-
     }
 }
 
